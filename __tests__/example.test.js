8f11d68169da25af54f4fbfdf01e8ab3701e9666
unknown <fredericobinsfeld@gmail.com>
2020-08-25 16:37:01 -0300

    Funcionalidade para adicionar usuários + testes

--- __tests__/example.test.js
+++
@@ -1,9 +0,0 @@
-function soma(a, b) {
-  return a + b;
-}
-
-test('se eu chamar a função soma 4 e 5 deve retornar 9.', () => {
-  const result = soma(4, 5);
-
-  expect(result).toBe(9);
-});
--- src/database/migrations/20200825172926-create-users.js
+++ src/database/migrations/20200825172926-create-users.js
@@ -1,23 +1,36 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
 };
